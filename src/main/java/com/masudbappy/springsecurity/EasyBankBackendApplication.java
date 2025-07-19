b2646ba26abb6e0bd7a474611a6c507a736037eb
masudbappy <masud.bappy90@gmail.com>
2025-07-19 18:30:24 +0600

    initiate spring security project

--- src/main/java/com/masudbappy/springsecurity/EasyBankBackendApplication.java
+++ src/main/java/com/masudbappy/springsecurity/EasyBankBackendApplication.java
@@ -2,12 +2,13 @@ package com.masudbappy.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringsecurityApplication {
+public class EasyBankBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringsecurityApplication.class, args);
+		SpringApplication.run(EasyBankBackendApplication.class, args);
 	}
 
 }
