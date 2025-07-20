0e7734ca4d31d6d081feff8dde8a8949c1fee3e4
masudbappy <masud.bappy90@gmail.com>
2025-07-20 15:03:27 +0600

    disable formLogin scenario.

--- src/main/java/com/masudbappy/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/masudbappy/springsecurity/config/ProjectSecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectSecurityConfig {
 
-    @Bean
+    /*@Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> {
                     requests.requestMatchers("/myAccount", "/myBalance",
@@ -18,6 +18,24 @@ public class ProjectSecurityConfig {
         });
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }*/
+    /*
+    If you want to disable formLogin,
+    This case is useful when your backend apis
+    are consumed by any other services;
+     */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            requests.requestMatchers("/myAccount", "/myBalance",
+                            "/myLoans", "/myCards").authenticated()
+                    .requestMatchers("/notice", "/contact", "/error").permitAll();
+        });
+        http.formLogin(httpSecurityFormLoginConfigurer ->
+                httpSecurityFormLoginConfigurer.disable());
+        http.httpBasic(httpSecurityHttpBasicConfigurer ->
+                httpSecurityHttpBasicConfigurer.disable());
         return http.build();
     }
 }
