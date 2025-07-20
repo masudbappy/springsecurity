7b96e6cbcf17c57f0da213a160611e3bb3016fce
masudbappy <masud.bappy90@gmail.com>
2025-07-20 23:36:51 +0600

    Implement Custom Registration

--- src/main/java/com/masudbappy/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/masudbappy/springsecurity/config/ProjectSecurityConfig.java
@@ -5,14 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -22,10 +18,11 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests((requests) -> {
                     requests.requestMatchers("/myAccount", "/myBalance",
                             "/myLoans", "/myCards").authenticated()
-                            .requestMatchers("/notice", "/contact", "/error").permitAll();
+                            .requestMatchers("/notice", "/contact", "/error", "/register").permitAll();
         });
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
     /*
