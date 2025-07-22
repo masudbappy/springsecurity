f14ddeb685997cd3728f5e538e8f294fdeef1a65
masudbappy <masud.bappy90@gmail.com>
2025-07-22 21:54:21 +0600

    Implementing and Customizing AuthenticationProvider

---
+++ src/main/java/com/masudbappy/springsecurity/config/EazyBankUsernamePwdAuthProvider.java
@@ -0,0 +1,47 @@
+package com.masudbappy.springsecurity.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EazyBankUsernamePwdAuthProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public EazyBankUsernamePwdAuthProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String userName = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            // Fetch age details and perform validation to check if age>18
+            return new UsernamePasswordAuthenticationToken(
+                    userDetails.getUsername(),
+                    userDetails.getPassword(),
+                    userDetails.getAuthorities()
+            );
+        } else {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
